96f7f3eb74405012b5345f446b810e06881f9880
Andreas Bernstein <andreas.bernstein@gmail.com>
2017-04-20 16:51:55 +0200

    Fix indentation

--- source/window.cpp
+++ source/window.cpp
@@ -19,10 +19,10 @@ Window::Window(std::pair<int, int> const& windowsize)
   glfwInit();
   //glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   glfwWindowHint(GLFW_RESIZABLE, 0);
   m_window = glfwCreateWindow(windowsize.first, windowsize.second, m_title.c_str(), nullptr, nullptr);
