918a266a60d59b7638c1e376bb83f42e40eb475d
PhiCecHey <65090580+PhiCecHey@users.noreply.github.com>
2020-05-25 20:44:22 +0200

    Add tests for Vec2

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,36 @@
+
+#include "vec2.hpp"
+
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+
+
+TEST_CASE("describe_vec2", "[vec2]") {
+	Vec2 a;
+	Vec2 b{ 5.1f, -9.3f };
+	REQUIRE(0.0f == Approx(a.x));
+	REQUIRE(0.0f == Approx(a.y));
+	REQUIRE(5.1f == Approx(b.x));
+	REQUIRE(-9.3f == Approx(b.y));
+}
+
+TEST_CASE("vec2 operators") {
+	Vec2 a;
+	Vec2 b{ 1.0f, 1.0f };
+	a += b;
+	REQUIRE(a.x == b.x);
+	REQUIRE(a.y == b.y);
+	a -= b;
+	REQUIRE(a.x == 0);
+	REQUIRE(a.y == 0);
+	b *= 0;
+	REQUIRE(b.x == 0);
+	REQUIRE(b.y == 0);
+	Vec2 c{ 2.0f, 2.0f };
+	c /= 2.0f;
+	REQUIRE(c.x == 1.0f);
+	REQUIRE(c.y == 1.0f);
+}
 
 int main(int argc, char *argv[])
 {
